773d19d01833ca7970f154db2fc96e1db7995b8d
bernardbdas <bernarddas1@gmail.com>
2022-02-11 12:01:19 +0530

    commit

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
-var app = angular.module('myApp', []);
+var myapp = angular.module('myApp', []);
 
 //to fetch data from json file
-app.controller('myCtrl', function($scope, $http) {
+myapp.controller('myCtrl', function($scope, $http) {
     $http.get("https://bernardbdas.github.io/wsd-lab8/users.json")
         .success(function(response) {
-            $scope.myData = response.data.user;
-            $scope.rowlimit = response.data.length();
+            $scope.myData = response.users;
+            $scope.rowlimit = response.users.length();
         });
-    $scope.myOrderBy = item;
+    //$scope.myOrderBy = item;
 });
 
 //custom filter 1
-app.filter('phnfilter', function() {
+myapp.filter('phnfilter', function() {
     return function(input) {
         return "+91-" + input;
     }
 });
 
 //custom filter 2
-app.filter("namefilter", function() {
+myapp.filter("namefilter", function() {
     return function(input) {
         return input.substring(0, 1).toUpperCase() + input.substring(1);
     }
 });
+
+function hw() {
+    alert("to check whether the script file is linking properly!");
+}
